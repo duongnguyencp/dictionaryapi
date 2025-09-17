296f6c8bd2537d7c15b481d20ef8498c0e92d286
duongcopeo <duongcopeozz@gmail.com>
2025-09-18 01:16:49 +0700

    c

--- src/middleware/cors.rs
+++ src/middleware/cors.rs
@@ -1,12 +1,10 @@
 use actix_cors::Cors;
 use actix_web::{http, web};
-use dotenv::dotenv;
-use std::env;
 
 use crate::AppState;
 pub fn init_cors(state: web::Data<AppState>) -> Cors {
-    dotenv().ok();
     let fe_endpoint_rs = state.config.front_end_point.clone();
+    println!("{}", fe_endpoint_rs.clone());
     Cors::default()
         .allowed_origin(&fe_endpoint_rs)
         .allowed_methods(vec!["GET"])
